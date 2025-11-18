075e0045908c0f8c07cfc55e461ef346cfa3dbd3
Girish Mogaveera <girizhhh@gmail.com>
2025-11-18 22:10:17 +0530

    localhost to api

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -46,8 +46,8 @@ export default function Home() {
     setIsLoading(true);
 
     try {
-      // const response = await fetch(`${BACKEND_URL}/ask`, {
-      const response = await fetch(`http://localhost:7860/ask`, {
+      const response = await fetch(`${BACKEND_URL}/ask`, {
+      // const response = await fetch(`http://localhost:7860/ask`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
